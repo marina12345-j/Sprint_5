aa7ccbd69efc105a997738bb6194819a63b4c09c
marina12345-j <jmailovamarina@yandex.ru>
2025-02-20 21:48:31 +0300

    доработка-1.2

--- tests/test_transition_rolls_sauces_fillings.py
+++ tests/test_transition_rolls_sauces_fillings.py
@@ -1,26 +1,23 @@
-from selenium.webdriver.common.by import By
+from locators import Locators
 
 
 # проверяем раздел Конструктор
 class TestTransitionRollsSaucesFillings:
     def test_transition_rolls(self,driver):
         # нажимаем на кнопку Булки (секция)
-        section_rolls = driver.find_element_by_xpath(By.XPATH, "//span[text() = 'Булки' and @class = 'text text_type_main-default']").click()
-        transition_rolls = driver.find_element(By.XPATH, "//h2[text() = 'Булки' and @class = 'text text_type_main-medium mb-6 mt-10")
-        assert section_rolls.text == 'Булки' and transition_rolls == 'Булки'
+        section_rolls = driver.find_element_by_xpath(Locators.CHAPTER_ROLLS).click()
+        transition_rolls = driver.find_element(Locators.TITTLE_ROLLS)
+        assert section_rolls.text == 'Булки' and transition_rolls.text == 'Булки'
 
     def test_transition_sauces(self, driver):
         # нажимаем на кнопку Соусы (секция)
-        section_souses = driver.find_element(By.XPATH, "//span[text() = 'Соусы' and @class = 'text text_type_main-default']").click()
+        section_souses = driver.find_element(Locators.CHAPTER_SAUCES).click()
 
-        transition_sauces = driver.find_element(By.XPATH, "//h2[text() = 'Соусы' and @class = 'text text_type_main-medium mb-6 mt-10']")
+        transition_sauces = driver.find_element(Locators.TITTLE_SAUCES)
         assert section_souses.text == 'Соусы' and transition_sauces.text == 'Соусы'
 
     def test_transition_fillings(self, driver):
         # нажимаем на кнопку Начинки (секция)
-        section_fillings = driver.find_element(By.XPATH, "//span[text() = 'Начинки' and @class = 'text text_type_main-default']").click()
-        transition_fillings = driver.find_element(By.XPATH, "//h2[text() = 'Начинки' and @class = 'text text_type_main-medium mb-6 mt-10']")
+        section_fillings = driver.find_element(Locators.CHAPTER_FILLINGS).click()
+        transition_fillings = driver.find_element(Locators.TITTLE_FILLINGS)
         assert section_fillings.text == 'Начинки' and transition_fillings.text == 'Начинки'
-
-
-
